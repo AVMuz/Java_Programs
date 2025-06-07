190e1ed7cc8be8643bae1a95558efc69a69d36e3
Anoop Verma <90458150+AVMuz@users.noreply.github.com>
2025-06-07 20:47:46 +0530

    Add files via upload

---
+++ Prog001.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a program to initialize variable with your name and diplays it.
+ *
+ * Anoop Verma
+ * 08-June-2025
+ */
+public class Prog001
+{
+    public static void main(String[] args)
+    {
+        String name = "Anoop Verma";
+        System.out.println(name);
+    }
+}
+
---
+++ Prog002.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a program to demonstrate different arithematical operartors.
+ *
+ * Anoop Verma
+ * 08-Jun-2025
+ */
+public class Prog002
+{
+    public static void main(String[] args)
+    {
+        int num1 = 12;
+        int num2 = 5;
+        
+        int sum = num1+num2;
+        int dif = num1-num2;
+        int mul = num1*num2;
+        int div = num1/num2;
+        int rem = num1%num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " = " + sum);
+        System.out.println("Differenece of " + num1 + " and " + num2 + " = " + dif);
+        System.out.println("Product " + num1 + " and " + num2 + " = " + mul);
+        System.out.println("Quotient of " + num1 + " and " + num2 + " = " + div);
+        System.out.println("Remainder of " + num1 + " and " + num2 + " = " + rem);
+    }
+}
